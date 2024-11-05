e47cf9f410b3e6edc09d45d09e5caa7997b1ffb9
Julius Türich <info@joonlabs.com>
2024-11-05 10:25:58 +0100

    add `cron_job` table

---
+++ database/migrations/2024_11_05_101815_create_cron_job_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cron_job', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("server_id")->constrained("server");
+            $table->string("name");
+            $table->string("command");
+            $table->enum("status", ["active", "inactive"])->default("active");
+            $table->string("minute")->nullable();
+            $table->string("hour")->nullable();
+            $table->string("day")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cron_job');
+    }
+};
