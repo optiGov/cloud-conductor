cd5273567683fb55341f2a7f8b7bdd52ccaab808
Julius Türich <info@joonlabs.com>
2024-11-27 08:13:30 +0100

    use new form components

--- app/Filament/Resources/JumpHostResource/RelationManagers/IpsecTunnelsRelationManager.php
+++ app/Filament/Resources/JumpHostResource/RelationManagers/IpsecTunnelsRelationManager.php
@@ -2,13 +2,23 @@
 
 namespace App\Filament\Resources\JumpHostResource\RelationManagers;
 
+use App\Filament\Forms\IPSecTunnel\IPSecTunnelFormEncryption;
+use App\Filament\Forms\IPSecTunnel\IPSecTunnelFormGeneral;
+use App\Filament\Forms\IPSecTunnel\IPSecTunnelFormLifetime;
+use App\Filament\Forms\IPSecTunnel\IPSecTunnelFormNetworking;
+use App\Models\DockerNetwork;
+use App\Models\JumpHost;
+use App\Models\LocalIpAddress;
+use App\Models\Server;
 use Filament\Forms;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Log;
 
 class IpsecTunnelsRelationManager extends RelationManager
 {
@@ -18,9 +28,34 @@ class IpsecTunnelsRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
+                IPSecTunnelFormGeneral::make(),
+                IPSecTunnelFormNetworking::make(),
+                IPSecTunnelFormEncryption::make(),
+                IPSecTunnelFormLifetime::make(),
+                Forms\Components\Section::make("Routing")
+                    ->compact()
+                    ->columns(1)
+                    ->schema([
+                        Repeater::make("routing")
+                            ->schema([
+                                Forms\Components\Select::make("local_network")
+                                    ->label("Local IP Address")
+                                    ->options(function (RelationManager $livewire) {
+                                        return JumpHost::findOrFail($livewire->ownerRecord->id)
+                                            ->localIpAddresses
+                                            ->mapWithKeys(function (LocalIpAddress $address) {
+                                                return [$address->id => "{$address->name} ({$address->ip_address})"];
+                                            })->toArray();
+                                    })
+                                    ->required(),
+                                Forms\Components\TextInput::make("remote_network")
+                                    ->placeholder("Remote Network")
+                                    ->maxLength(255)
+                                    ->required(),
+                            ])
+                            ->default([])
+                            ->columns(2),
+                    ])
             ]);
     }
 
@@ -30,6 +65,8 @@ class IpsecTunnelsRelationManager extends RelationManager
             ->recordTitleAttribute('name')
             ->columns([
                 Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('remote_ip'),
+                Tables\Columns\TextColumn::make('ike_version'),
             ])
             ->filters([
                 //
--- app/Filament/Resources/ServerResource/RelationManagers/IpsecTunnelsRelationManager.php
+++ app/Filament/Resources/ServerResource/RelationManagers/IpsecTunnelsRelationManager.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Resources\ServerResource\RelationManagers;
 
+use App\Filament\Forms\IPSecTunnel\IPSecTunnelFormEncryption;
+use App\Filament\Forms\IPSecTunnel\IPSecTunnelFormGeneral;
+use App\Filament\Forms\IPSecTunnel\IPSecTunnelFormLifetime;
+use App\Filament\Forms\IPSecTunnel\IPSecTunnelFormNetworking;
 use App\Filament\Resources\ServerResource\RelationManagers\Actions\ActionRMIPSecTunnelsApply;
 use App\Filament\Resources\ServerResource\RelationManagers\Actions\ActionRMIPSecTunnelsStart;
 use App\Models\DockerNetwork;
@@ -27,151 +31,10 @@ class IpsecTunnelsRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\Section::make("General")
-                    ->compact()
-                    ->columns(2)
-                    ->schema([
-                        Forms\Components\Grid::make()
-                            ->columns(1)
-                            ->schema([
-                                Forms\Components\TextInput::make("name")
-                                    ->required()
-                                    ->maxLength(255),
-                            ]),
-                        Forms\Components\Grid::make()
-                            ->columns(2)
-                            ->schema([
-                                Forms\Components\Select::make("ike_version")
-                                    ->options([
-                                        "v1" => "Version 1",
-                                        "v2" => "Version 2",
-                                    ])
-                                    ->default("v2")
-                                    ->required(),
-                                Forms\Components\TextInput::make("psk")
-                                    ->default(fn() => Str::random(60))
-                                    ->required()
-                                    ->maxLength(255),
-                            ])
-                    ]),
-                Forms\Components\Section::make("Networking")
-                    ->compact()
-                    ->columns(2)
-                    ->schema([
-                        Forms\Components\TextInput::make("local_ip")
-                            ->default(fn(RelationManager $livewire) => $livewire->ownerRecord->host)
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make("remote_ip")
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make("local_id")
-                            ->placeholder("Keep empty to use local IP")
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make("remote_id")
-                            ->placeholder("Keep empty to use remote IP")
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make("local_subnet")
-                            ->default("0.0.0.0/0")
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make("remote_subnet")
-                            ->default("0.0.0.0/0")
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\Checkbox::make("separate_connections")
-                            ->columnSpanFull()
-                            ->label("Separate Connections")
-                            ->hint("If enabled, a separate tunnel config for each remote subnet will be created."),
-                    ]),
-                Forms\Components\Section::make("Encryption, Hashing and Diffie-Hellman-Group")
-                    ->compact()
-                    ->columns(3)
-                    ->schema([
-                        Forms\Components\TextInput::make("ike_encryption")
-                            ->default("aes256")
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make("ike_hash")
-                            ->default("sha2_512")
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\Select::make("ike_dh_group")
-                            ->options([
-                                "modp768" => "1 (modp768)",
-                                "modp1024" => "2 (modp1024)",
-                                "modp1536" => "5 (modp1536)",
-                                "modp2048" => "14 (modp2048)",
-                                "modp3072" => "15 (modp3072)",
-                                "modp4096" => "16 (modp4096)",
-                                "modp6144" => "17 (modp6144)",
-                                "modp8192" => "18 (modp8192)",
-                                "ecp256" => "19 (ecp256)",
-                                "ecp384" => "20 (ecp384)",
-                                "ecp521" => "21 (ecp521)",
-                                "modp1024s160" => "22 (modp1024s160)",
-                                "modp2048s224" => "23 (modp2048s224)",
-                                "modp2048s256" => "24 (modp2048s256)",
-                                "ecp192" => "25 (ecp192)",
-                                "ecp224" => "26 (ecp224)",
-                                "ecp224bp" => "27 (ecp224bp)",
-                                "ecp256bp" => "28 (ecp256bp)",
-                                "ecp384bp" => "29 (ecp384bp)",
-                                "ecp512bp" => "30 (ecp512bp)",
-                                "curve25519" => "31 (curve25519)",
-                                "curve448" => "32 (curve448)",
-                            ])
-                            ->default("ecp256")
-                            ->required(),
-                        Forms\Components\TextInput::make("esp_encryption")
-                            ->default("aes256")
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make("esp_hash")
-                            ->default("sha2_512")
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\Select::make("esp_dh_group")
-                            ->options([
-                                "modp768" => "1 (modp768)",
-                                "modp1024" => "2 (modp1024)",
-                                "modp1536" => "5 (modp1536)",
-                                "modp2048" => "14 (modp2048)",
-                                "modp3072" => "15 (modp3072)",
-                                "modp4096" => "16 (modp4096)",
-                                "modp6144" => "17 (modp6144)",
-                                "modp8192" => "18 (modp8192)",
-                                "ecp256" => "19 (ecp256)",
-                                "ecp384" => "20 (ecp384)",
-                                "ecp521" => "21 (ecp521)",
-                                "modp1024s160" => "22 (modp1024s160)",
-                                "modp2048s224" => "23 (modp2048s224)",
-                                "modp2048s256" => "24 (modp2048s256)",
-                                "ecp192" => "25 (ecp192)",
-                                "ecp224" => "26 (ecp224)",
-                                "ecp224bp" => "27 (ecp224bp)",
-                                "ecp256bp" => "28 (ecp256bp)",
-                                "ecp384bp" => "29 (ecp384bp)",
-                                "ecp512bp" => "30 (ecp512bp)",
-                                "curve25519" => "31 (curve25519)",
-                                "curve448" => "32 (curve448)",
-                            ])
-                            ->default("ecp256")
-                            ->required(),
-                    ]),
-                Forms\Components\Section::make("Lifetime")
-                    ->compact()
-                    ->columns(2)
-                    ->schema([
-                        Forms\Components\TextInput::make("ike_lifetime")
-                            ->default(28800)
-                            ->required()
-                            ->type("number"),
-                        Forms\Components\TextInput::make("key_lifetime")
-                            ->default(3600)
-                            ->required()
-                            ->type("number"),
-                    ]),
+                IPSecTunnelFormGeneral::make(),
+                IPSecTunnelFormNetworking::make(),
+                IPSecTunnelFormEncryption::make(),
+                IPSecTunnelFormLifetime::make(),
                 Forms\Components\Section::make("Routing")
                     ->compact()
                     ->columns(1)
@@ -183,7 +46,6 @@ class IpsecTunnelsRelationManager extends RelationManager
                                         return Server::findOrFail($livewire->ownerRecord->id)
                                             ->dockerNetworks
                                             ->mapWithKeys(function (DockerNetwork $network) {
-                                                Log::log("debug", $network);
                                                 return [$network->id => "{$network->name} ($network->subnet)"];
                                             })->toArray();
                                     })
