148e6b5a9eead806591557f52d474004b7a092ef
Julius Türich <info@joonlabs.com>
2024-11-27 08:01:51 +0100

    update stats

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\CronJob;
+use App\Models\DockerContainer;
+use App\Models\IPSecTunnel;
 use App\Models\JumpHost;
 use App\Models\Key;
 use App\Models\Server;
@@ -13,10 +16,6 @@ class StatsOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Keys', Key::count())
-                ->description('Total keys managed by the system.')
-                ->url('/admin/keys')
-                ->icon('heroicon-o-key'),
             Stat::make('Servers', Server::count())
                 ->description('Total servers managed by the system.')
                 ->url('/admin/servers')
@@ -25,6 +24,22 @@ class StatsOverview extends BaseWidget
                 ->url('/admin/jump-hosts')
                 ->description('Total jump hosts managed by the system.')
                 ->icon('heroicon-o-forward'),
+            Stat::make('IPSec-Tunnels', IPSecTunnel::count())
+                ->description('Total IPSec tunnels managed by the system.')
+                ->url('/admin/server')
+                ->icon('heroicon-o-shield-check'),
+            Stat::make('Containers', DockerContainer::count())
+                ->description('Total docker containers managed by the system.')
+                ->url('/admin/server')
+                ->icon('heroicon-o-cube'),
+            Stat::make('Networks', DockerContainer::count())
+                ->description('Total docker networks managed by the system.')
+                ->url('/admin/server')
+                ->icon('heroicon-o-globe-alt'),
+            Stat::make('Cron Jobs', CronJob::count())
+                ->description('Total cron jobs managed by the system.')
+                ->url('/admin/server')
+                ->icon('heroicon-o-clock'),
         ];
     }
 }
